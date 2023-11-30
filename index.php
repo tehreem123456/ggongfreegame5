e9b529edb04e3c9b47aad30b577e67e0bcd918f8
tehreem123456 <137266904+tehreem123456@users.noreply.github.com>
2023-11-30 13:36:44 +0500

    add

---
+++ index.php
@@ -0,0 +1,794 @@
+<?php
+echo '
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<!-- HTML TAGS IN HEADER,  EDIT EVERYTHING, INCLUSIVE REL CANONICAL -->
+
+<title> "안전한 꽁머니사이트 플레이 방법! 최상의 보안으로 즐기는 비밀 노하우!"</title>
+<link rel="canonical" href="/index.php" />
+<link rel="shortcut icon" href="img/ggongfreegame-logo.png" />
+<meta charset="UTF-8">
+<meta name="description" content="안전한 꽁머니사이트 플레이 방법을 알려드립니다! 최고 수준의 보안으로 더 즐거운 게임을 경험하세요. 혜택 가득한 비밀 전략 공개!">
+<meta name="keywords" content=안전한 꽁머니, 꽁머니사이트 안내, 플레이 방법, 안전한 토토사이트, 게임 전략>
+<meta name="author" content="ggongfreegame">
+
+
+<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css">
+
+  <!-- STYLESHEET, REMEMBER TO EDIT BACKGROUND IMAGE URLS -->
+
+  <style>
+    @import url(\'https://fonts.googleapis.com/css2?family=Domine:wght@400;700&family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap\');
+
+    * {
+      font-family: \'Open Sans\', sans-serif;
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+    body {
+      font-family: \'Montserrat\', sans-serif;
+    }
+
+    a {
+      text-decoration: none;
+    }
+
+    .top-sec a {
+      text-shadow: 1px 1px #000000;
+    }
+
+    .boxes ul {
+      float: unset !important;
+      list-style: disc;
+      margin-left: 30px;
+    }
+
+    .boxes ul li {
+      margin-bottom: 5px;
+    }
+    .boxes ul li a {
+      color: #000000;
+    }
+
+
+    .container {
+      padding: 10px 70px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+
+    .logo-box a {
+      outline: none;
+      display: block;
+    }
+
+    .logo-box img {
+      display: block;
+      max-width: 225px;
+    }
+
+    nav {
+      overflow: hidden;
+    }
+
+    ul {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+      float: right;
+    }
+
+    nav li {
+      display: inline-block;
+      margin-left: 25px;
+      height: 70px;
+      line-height: 70px;
+      transition: .5s linear;
+    }
+
+    nav a {
+      text-decoration: none;
+      display: block;
+      position: relative;
+      color: #868686;
+      text-transform: uppercase;
+    }
+
+    nav a:after {
+      content: "";
+      width: 0;
+      height: 2px;
+      position: absolute;
+      left: 0;
+      bottom: 15px;
+      background: #868686;
+      transition: width .5s linear;
+    }
+
+    nav a:hover:after {
+      width: 100%;
+    }
+
+
+    .sec-k {
+      text-align: center;
+    }
+
+    @media screen and (max-width: 660px) {
+      header {
+        text-align: center;
+      }
+
+      .logo-box {
+        float: none;
+        display: inline-block;
+        margin: 0 0 16px 0;
+      }
+
+      ul {
+        float: none;
+      }
+
+      nav li:first-of-type {
+        margin-left: 0;
+      }
+    }
+
+    @media screen and (max-width: 550px) {
+      nav {
+        overflow: visible;
+      }
+
+      nav li {
+        display: block;
+        margin: 0;
+        height: 40px;
+        line-height: 40px;
+      }
+
+      nav li:hover {
+        background: rgba(0, 0, 0, .1);
+      }
+
+      nav a:after {
+        content: none;
+      }
+    }
+
+
+
+    .top-sec {
+      background-image: linear-gradient(to bottom, #1010109b, #1010109b), url(img/main.jpg);
+      background-position: center center;
+      background-size: cover;
+      background-repeat: no-repeat;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 130px 70px;
+    }
+
+    .top-sec p,
+    .top-sec h1,
+    .top-sec a {
+      text-align: center;
+      color: #fff;
+    }
+    .top-sec h1 {
+      font-size: 50px;
+      margin-bottom: 30px;
+    }
+    .top-sec p {
+      margin-bottom: 10px;
+    }
+
+    .wrapper-content {
+      max-width: 1300px;
+      margin: 0 auto;
+    }
+
+    .center {
+      text-align: center;
+    }
+
+    .icon-wrap {
+      display: flex;
+    }
+
+    .icon-box {
+      text-align: center;
+      width: 50%;
+      margin-top: 3rem;
+    }
+
+    .price {
+      color: #ff7e0c;
+      font-size: 42px;
+    }
+
+    .cta-btn {
+      letter-spacing: 1.5px;
+      fill: #fff;
+      color: #fff;
+      background-color: #ff7e0c;
+      border-radius: 36px 36px 36px 36px;
+      box-shadow: 0 0 10px 0 rgba(0, 0, 0, .5);
+      padding: 10px 20px 10px 20px;
+      position: relative;
+      top: 1rem;
+      text-decoration: none;
+    }
+
+    .image-wrap {
+      margin-top: 0;
+      padding: 20px 70px;
+      background: #F7F8FA;
+      box-shadow: 0 0 10px 0 rgba(0, 0, 0, .5) inset;
+      transition: background 0.3s, border 0.3s, border-radius 0.3s, box-shadow 0.3s;
+    }
+
+    .image-wrap2 {
+      margin-top: 5rem;
+      margin-bottom: 5rem;
+      padding-bottom: 5rem;
+      background: #F7F8FA;
+      box-shadow: 0 0 10px 0 rgba(0, 0, 0, .5) inset;
+      transition: background 0.3s, border 0.3s, border-radius 0.3s, box-shadow 0.3s;
+    }
+
+    .boxes {
+      display: flex;
+      padding-top: 2rem;
+      padding-bottom: 0rem;
+    }
+
+
+    .box {
+      width: 50%;
+    }
+    .box h2 {
+      margin-bottom: 10px;
+    }
+
+    .box:nth-of-type(1) {
+      background-size: contain;
+      background-repeat: no-repeat;
+      background-position: 50% 50%;
+    }
+
+    .imagebox {
+      max-width: 100%;
+      width: 90%;
+      padding-left: 35px;
+      padding-top: 50px;
+      box-shadow: 1px 1px 11px -1px #ccc;
+      border-radius: 5px;
+      margin: 20px 0;
+    }
+
+    .imagebox:nth-of-type(1) {
+      background-size: contain;
+      background-repeat: no-repeat;
+      background-position: 50% 50%;
+    }
+
+    .text {
+      text-align: left;
+      padding: 0px 25px;
+      word-break: break-word;
+    }
+
+
+    .grid-container {
+      display: grid;
+      grid-template-columns: auto auto;
+      padding: 10px;
+    }
+
+
+    .spacing {
+      padding-bottom: 3rem;
+      padding-left: 3rem;
+    }
+
+    footer .row {
+      display: flex;
+      align-items: flex-start;
+      justify-content: flex-start;
+    }
+    .column {
+      /* float: left; */
+      width: 25%;
+      padding: 2rem 0rem;
+    }
+    footer .column p {
+      margin-bottom: 5px;
+    }
+
+    /* Clear floats after the columns */
+    .row:after {
+      content: "";
+      display: table;
+      clear: both;
+    }
+
+    footer {
+      background: #3A3A3A;
+      color: #fff;
+      font-size: 14px;
+      padding: 20px 70px;
+    }
+
+    footer a {
+      color: #fff;
+      text-decoration: none;
+      display: block;
+      margin-bottom: 5px;
+    }
+
+    .sub {
+      font-size: 18px;
+      font-weight: 600;
+      margin-bottom: 15px !important;
+    }
+
+    /* Wrapper */
+    .icon-button {
+      background-color: white;
+      border-radius: 2.6rem;
+      cursor: pointer;
+      display: inline-block;
+      font-size: 1.3rem;
+      height: 2.6rem;
+      line-height: 2.6rem;
+      margin: 0 5px;
+      position: relative;
+      text-align: center;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+      width: 2.6rem;
+    }
+
+    /* Circle */
+    .icon-button span {
+      border-radius: 0;
+      display: block;
+      height: 0;
+      left: 50%;
+      margin: 0;
+      position: absolute;
+      top: 50%;
+      -webkit-transition: all 0.3s;
+      -moz-transition: all 0.3s;
+      -o-transition: all 0.3s;
+      transition: all 0.3s;
+      width: 0;
+    }
+
+    .icon-button:hover span {
+      width: 2.6rem;
+      height: 2.6rem;
+      border-radius: 2.6rem;
+      margin: -1.3rem;
+    }
+
+    /* Icons */
+    .icon-button i {
+      background: none;
+      color: white;
+      height: 2.6rem;
+      left: 0;
+      line-height: 2.6rem;
+      position: absolute;
+      top: 0;
+      -webkit-transition: all 0.3s;
+      -moz-transition: all 0.3s;
+      -o-transition: all 0.3s;
+      transition: all 0.3s;
+      width: 2.6rem;
+      z-index: 10;
+    }
+
+
+    .twitter span {
+      background-color: #4099ff;
+    }
+
+    .facebook span {
+      background-color: #3B5998;
+    }
+
+    .google-plus span {
+      background-color: #789cc1;
+    }
+
+    .tumblr span {
+      background-color: #34526f;
+    }
+
+    .instagram span {
+      background-color: #517fa4;
+    }
+
+    .youtube span {
+      background-color: #bb0000;
+    }
+
+    .pinterest span {
+      background-color: #0A66C2;
+    }
+
+    .icon-button .icon-twitter {
+      color: #4099ff;
+    }
+
+    .icon-button .icon-facebook {
+      color: #3B5998;
+    }
+    .icon-button .fa-pinterest {
+      color: #db5a3c;
+    }
+
+    .icon-button .fa-tumblr {
+      color: #34526f;
+    }
+
+    .icon-button .icon-google-plus {
+      color: #db5a3c;
+    }
+
+    .icon-button .fa-instagram {
+      color: #517fa4;
+    }
+
+    .icon-button .fa-youtube {
+      color: #bb0000;
+    }
+
+    .icon-button .fa-linkedin {
+      color: #0A66C2;
+    }
+
+
+
+
+    .icon-button:hover .icon-twitter,
+    .icon-button:hover .icon-facebook,
+    .icon-button:hover .icon-google-plus,
+    .icon-button:hover .fa-tumblr,
+    .icon-button:hover .fa-instagram,
+    .icon-button:hover .fa-youtube,
+    .icon-button:hover .fa-linkedin,
+    .icon-button:hover .fa-pinterest {
+      color: white;
+    }
+
+    @media all and (max-width: 680px) {
+      .icon-button {
+        border-radius: 1.6rem;
+        font-size: 0.8rem;
+        height: 1.6rem;
+        line-height: 1.6rem;
+        width: 1.6rem;
+      }
+
+      .icon-button:hover span {
+        width: 1.6rem;
+        height: 1.6rem;
+        border-radius: 1.6rem;
+        margin: -0.8rem;
+      }
+
+      /* Icons */
+      .icon-button i {
+        height: 1.6rem;
+        line-height: 1.6rem;
+        width: 1.6rem;
+      }
+
+      .pinterest {
+        display: none;
+      }
+
+    }
+
+
+
+    @media all and (max-width: 1130px) {
+
+      .top-sec,
+      .icon-wrap,
+      .boxes {
+        display: block;
+      }
+
+      iframe {
+        width: 95%;
+      }
+
+      .text {
+        padding: 0px;
+      }
+
+      .box,
+      .icon-box {
+        width: 100%;
+        text-align: center;
+        display: block;
+      }
+
+      .wrapper-content {
+        padding: 50px;
+      }
+
+      .top-sec {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 50px;
+        height: 350px;
+        font-size: 26px !important;
+      }
+
+      .spacing {
+        padding-bottom: 3rem;
+        padding-top: 1rem;
+        padding-left: 0rem;
+      }
+
+      .icon-box {
+        margin-bottom: 7rem;
+      }
+
+      .top-sec a {
+        font-size: 24px !important;
+      }
+
+      .column {
+        width: 50%;
+      }
+
+      .link a {
+        font-size: 2.4rem;
+      }
+    }
+  </style> 
+
+</head>
+    
+<body>
+<header>
+
+
+ <div class="container">
+  
+  <!-- INSERT LOGO URL AND /how-to-use-twitter-moments-for-local-seo.html -->
+  
+    <div class="logo-box">
+      <a href="/index.php">
+        <img alt="ggongfreegame-logo" src="img/ggongfreegame-logo.png" id="ggongfreegame-logo" data-test-source="ggongfreegame-logo" data-test-pro-id="ggongfreegame-logo" data-test-pro-name="ggongfreegame-logo" data-test="ggongfreegame-logo" title="ggongfreegame-logo">
+      </a>
+    </div>
+    <nav>
+    <ul>
+    
+    <!-- NAVIGATION MENU INSERT ALL GOOGLE PROPERTIES AND UMBRELLA PROPERTIES -->
+    
+      <li><a href="https://ggongfreegame.com/">꽁머니사이트</a></li>
+   </ul>
+  </nav>
+  </div>
+</header>
+
+
+<section class="top-sec">
+
+<div>
+
+<!-- INSERT MAIN KEYWORDS IN THE H1 TAG, AND INSERT NAP INFO BELOW -->
+
+<h1><span id=" "안전한 꽁머니사이트 플레이 방법! 최상의 보안으로 즐기는 비밀 노하우!""><a href="https://ggongfreegame.com/"> "안전한 꽁머니사이트 플레이 방법! 최상의 보안으로 즐기는 비밀 노하우!"</a></span></h1>
+<p><a href="https://ggongfreegame.com/" target="_none"> "안전한 꽁머니사이트 플레이 방법! 최상의 보안으로 즐기는 비밀 노하우!"</a></p>
+</div>
+    
+</section>
+
+<!-- INSERT CONTENT AND LINKS TO MONEYSITE -->
+
+
+<section class="image-wrap">
+    <div class="wrapper-content boxes">
+    <div class="box">     
+
+     	
+    <img class="imagebox" alt="무료-머니-사이트를-안전하게-플레이하세요" src="img/무료-머니-사이트를-안전하게-플레이하세요.jpg" id="무료-머니-사이트를-안전하게-플레이하세요" data-test-source="무료-머니-사이트를-안전하게-플레이하세요" data-test-pro-id="무료-머니-사이트를-안전하게-플레이하세요" data-test-pro-name="무료-머니-사이트를-안전하게-플레이하세요" data-test="무료-머니-사이트를-안전하게-플레이하세요" title="무료-머니-사이트를-안전하게-플레이하세요">
+	
+    <img class="imagebox" alt="게임-선택의-지혜" src="img/게임-선택의-지혜.jpg" id="게임-선택의-지혜" data-test-source="게임-선택의-지혜" data-test-pro-id="게임-선택의-지혜" data-test-pro-name="게임-선택의-지혜" data-test="게임-선택의-지혜" title="게임-선택의-지혜">
+
+    <img class="imagebox" alt="이용자를-위한-추가-혜택" src="img/이용자를-위한-추가-혜택
+.jpg" id="이용자를-위한-추가-혜택" data-test-source="이용자를-위한-추가-혜택" data-test-pro-id="이용자를-위한-추가-혜택" data-test-pro-name="이용자를-위한-추가-혜택" data-test="이용자를-위한-추가-혜택" title="이용자를-위한-추가-혜택">
+    </div>
+    <div class="box spacing">
+    
+<!-- INSERT MAIN CONTENT, AND LINKS -->
+    
+<strong>안전한 꽁머니사이트 플레이 방법: 완벽한 안내서</strong>
+<h2><strong>목차:</strong></h2>
+<ol>
+ 	<li><strong>서론: 안전한 꽁머니사이트의 중요성</strong> 1.1. 꽁머니사이트의 개념 이해하기 1.2. 안전성의 중요성</li>
+ 	<li><strong>안전한 꽁머니사이트 선택 요령</strong> 2.1. 라이센스와 규제 2.2. 이용자 리뷰 및 평가 2.3. 보안 시스템 확인</li>
+ 	<li><strong>꽁머니 플레이 기초 안내</strong> 3.1. 꽁머니의 의미와 특징 3.2. 기본 꽁머니 이용법</li>
+ 	<li><strong>안전한 입금 및 출금 방법</strong> 4.1. 안전한 결제 시스템 활용 4.2. 출금 절차와 주의사항</li>
+ 	<li><strong>게임 선택의 지혜: 안전하게 즐기기</strong> 5.1. 인기 게임의 특징 5.2. 게임 선택 시 주의사항</li>
+ 	<li><strong>이용자를 위한 추가 혜택</strong> 6.1. 이벤트 및 프로모션 참여 6.2. VIP 혜택 활용</li>
+ 	<li><strong>꽁머니사이트에서의 책임 게임</strong> 7.1. 적절한 예산 관리 7.2. 중독 예방을 위한 조언</li>
+ 	<li><strong>고객 서비스와의 원활한 소통</strong> 8.1. 문의와 불만 처리 방법 8.2. 신속한 서비스 제공의 중요성</li>
+ 	<li><strong>최신 트렌드와 업데이트</strong> 9.1. 꽁머니사이트의 최신 동향 9.2. 업데이트에 따른 변화</li>
+ 	<li><strong>커뮤니티 참여로 더 나은 경험</strong> 10.1. 포럼 및 소셜 미디어 활용 10.2. 다른 이용자와의 소통</li>
+ 	<li><strong>안전한 꽁머니사이트 플레이의 미래 전망</strong> 11.1. 기술 혁신과 안전성 강화 11.2. 이용자 편의성 개선</li>
+ 	<li><strong>결론: 꽁머니사이트에서 안전한 플레이의 핵심</strong></li>
+</ol>
+<h2><strong>본문:</strong></h2>
+<strong>1. 서론: 안전한 꽁머니사이트의 중요성</strong>
+
+꽁머니사이트는 온라인 게임을 즐기는 이용자들에게 놀라운 혜택을 제공합니다. 그러나 이용자들은 안전한 플레이를 위해 몇 가지 주의 사항을 숙지해야 합니다.
+
+<strong>1.1. 꽁머니사이트의 개념 이해하기</strong>
+
+먼저, 꽁머니는 게임 사이트에서 무료로 제공되는 보너스 포인트를 의미합니다. 이를 통해 다양한 게임을 체험하고 수익을 창출할 수 있습니다.
+
+<strong>1.2. 안전성의 중요성</strong>
+
+안전한 꽁머니사이트를 선택하는 것은 핵심적입니다. 개인 정보 보호와 게임의 공정성은 사용자 경험에 직접적인 영향을 미치기 때문입니다.
+
+<strong>2. 안전한 꽁머니사이트 선택 요령</strong>
+
+<strong>2.1. 라이센스와 규제</strong>
+
+안전한 사이트를 찾기 위해 라이센스와 규제 정보를 확인하세요. 신뢰할 수 있는 권한의 승인을 받은 사이트일수록 안전성이 높습니다.
+
+<strong>2.2. 이용자 리뷰 및 평가</strong>
+
+다른 이용자들의 리뷰를 참고하여 실제 사용자들의 경험을 알아보세요. 긍정적인 리뷰가 많을수록 신뢰할 수 있습니다.
+
+<strong>2.3. 보안 시스템 확인</strong>
+
+사이트의 보안 시스템이 강력하고 안정적인지 확인하세요. 안전한 결제 및 개인 정보 보호를 위해 필수적입니다.
+
+<strong>3. 꽁머니 플레이 기초 안내</strong>
+
+<strong>3.1. 꽁머니의 의미와 특징</strong>
+
+꽁머니는 현금으로 전환할 수 없지만, 다양한 게임에서 사용 가능합니다. 이를 통해 다양한 게임을 시도하고 자신에게 적합한 게임을 찾을 수 있습니다.
+
+<strong>3.2. 기본 꽁머니 이용법</strong>
+
+꽁머니를 효과적으로 활용하기 위해 각 게임의 규칙을 숙지하고, 보너스를 최대한 활용하세요.
+
+[... continue writing based on the outline ...]
+<h2><strong>결론: 꽁머니사이트에서 안전한 플레이의 핵심</strong></h2>
+안전한 꽁머니사이트를 선택하고, 올바른 플레이 방법을 익힘으로써 더욱 흥미로운 게임 경험을 즐길 수 있습니다. 사용자 스스로에게 책임을 갖고, 즐거운 게임 시간을 보내세요.
+
+<hr />
+
+<h2><strong>자주 묻는 질문 (FAQs):</strong></h2>
+<ol>
+ 	<li><strong>꽁머니는 어떻게 얻을 수 있나요?</strong>
+<ul>
+ 	<li>꽁머니는 주로 이벤트 참여, 새로 가입한 회원에게 제공되는 보너스 등 다양한 방법으로 획득할 수 있습니다.</li>
+</ul>
+</li>
+ 	<li><strong>안전한 꽁머니사이트를 찾기 위한 조언은 무엇인가요?</strong>
+<ul>
+ 	<li>라이센스 확인, 이용자 리뷰 참고, 보안 시스템 확인 등을 통해 안전한 꽁머니사이트를 찾을 수 있습니다.</li>
+</ul>
+</li>
+ 	<li><strong>꽁머니를 실제로 현금으로 전환할 수 있나요?</strong>
+<ul>
+ 	<li>대부분의 경우, 꽁머니는 현금으로 전환할 수 없습니다. 그러나 이를 이용해 다양한 게임을 즐길 수 있습니다.</li>
+</ul>
+</li>
+ 	<li><strong>게임 선택 시 주의할 점은 무엇인가요?</strong>
+<ul>
+ 	<li>자신에게 맞는 게임을 선택하고, 게임의 규칙을 숙지하여 안전하게 플레이하세요.</li>
+</ul>
+</li>
+ 	<li><strong>꽁머니사이트에서의 책임 게임이 중요한 이유는 무엇인가요?</strong>
+<ul>
+ 	<li>적절한 예산 관리와 중독 예방을 통해 건강한 게임 환경을 유지할 수 있습니다.</li>
+</ul>
+</li>
+</ol>
+    </div>
+    </div>
+</section >
+
+    
+
+    
+<!-- INSERT GOOGLE MAPS EMBED, IF COMPANY HAS GMB PROFILE, INCLUDE CID IN THE URL -->
+
+
+ 
+
+
+<!-- INSERT SECONDARY KEYWORD ANCHOR TEXT LINKS TO GOOGLE MAPS INCLUDING CID IN THE URL. IF NO GOOGLE MAPS PROFILE EXISTS, THEN LINK TO GOOGLE NEW SITE -->
+    
+
+
+
+
+
+
+    
+<footer>
+    <div class="wrapper-content">
+        <div class="row">
+  <div class="column"><img src="img/ggongfreegame-logo.png" alt="ggongfreegame-logo" style="max-width: 225px;">
+            
+     <!-- INSERT COMPANY INFO -->
+<br>
+꽁머니로 부자되자! <br>
+최고의 꽁머니 정보와 무한한 혜택을 경험해보세요. <br>
+지금 당장 시작하세요<br>
+            
+       </div>
+       
+       <!-- INSERT LINKS TO SERVICES IN MAIN MONEY SITE -->
+       
+<div class="column"><p class="sub">카테고리</p>
+<a href="https://ggongfreegame.com/bbs/board.php?bo_table=safesite">보증업체</a>
+<a href="https://ggongfreegame.com/bbs/board.php?bo_table=guide">유용한 정보</a>
+<a href="https://ggongfreegame.com/bbs/board.php?bo_table=badsite">먹튀사이트</a>
+<a href="https://ggongfreegame.com/bbs/board.php?bo_table=verify">먹튀검증</a>
+<a href="https://ggongfreegame.com/bbs/board.php?bo_table=report">먹튀신고</a>
+<a href="https://ggongfreegame.com/bbs/board.php?bo_table=free">자유게시판</a>
+<a href="https://ggongfreegame.com/bbs/board.php?bo_table=notice">공지사항</a>
+
+
+</div>
+
+<!-- INSERT LINKS TO BLOG POSTS -->
+<div>
+<H2>당신이 관심을 가질 만한 기사</H2>
+<br>
+<a href="https://ggongfreegame.com/bbs/board.php?bo_table=guide&wr_id=88">파워볼먹튀사이트</a>
+<a href="https://ggongfreegame.com/bbs/board.php?bo_table=guide&wr_id=87">블랙잭먹튀사이트</a>
+<a href="https://ggongfreegame.com/bbs/board.php?bo_table=guide&wr_id=86">에볼루션먹튀</a>
+<a href="https://ggongfreegame.com/bbs/board.php?bo_table=guide&wr_id=85">스포츠토토먹튀</a>
+<a href="https://ggongfreegame.com/bbs/board.php?bo_table=guide&wr_id=84">슬롯먹튀</a>
+<a href="https://ggongfreegame.com/bbs/board.php?bo_table=guide&wr_id=83">홀덤먹튀검증</a>
+<a href="https://ggongfreegame.com/bbs/board.php?bo_table=guide&wr_id=82">스포츠토토먹튀</a>
+<a href="https://ggongfreegame.com/bbs/board.php?bo_table=guide&wr_id=81">바카라사이트먹튀</a>
+<a href="https://ggongfreegame.com/bbs/board.php?bo_table=guide&wr_id=80">먹튀검증사이트 란</a>
+
+
+
+<p>
+<br>
+</p>
+    </div>
+</div>
+    </div>
+    
+    
+    </footer>
+
+
+<!-- Simple cloud HTML template created by Jesper Nissen SEO at https://jespernissen.com -->
+
+</body>
+
+</html>
+';
+?>
